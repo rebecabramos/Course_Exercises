37356d39081dd4670a39a12050996527dc60fe52
Rebeca Raab Bias Ramos <41303692+rebecabramos@users.noreply.github.com>
2021-10-07 11:15:22 -0300

    Create Hacking_Version_1.py
    Version 1.0 of the first game proposed by the course.

---
+++ Week 3/Hacking_Version_1.py
@@ -0,0 +1,38 @@
+# Hacking Version 1
+# This is a text-based passoword guessing game that displays a
+# list of potential computer passwords. The player is allowed
+# 1 attempt to guess the passoword. The game indicates that the 
+# player failed to guess the passoword correctly.
+
+# display header
+print('DEBUG MODE')
+print('1 ATTEMPT(S) LEFT')
+print('')
+
+# display passoword
+print('PROVIDE')
+print('SETTING')
+print('CANTINA')
+print('CUTTING')
+print('HUNTERS')
+print('SURVIVE')
+print('HEARING')
+print('HUNTING')
+print('REALIZE')
+print('NOTHING')
+print('OVERLAP')
+print('FINDING')
+print('PUTTING')
+print('')
+
+# prompt for guess
+input('Enter password >')
+
+# end game
+print('LOGIN FAILURE - TERMINAL LOCKED')
+print('')
+
+print('PLEASE CONTACT AN ADMINISTRATOR')
+print('')
+
+print('PRESS ENTER TO EXIT')
