fb4ffef53af9299cd416ffc426abcd288733dca3
Rebeca Raab Bias Ramos <41303692+rebecabramos@users.noreply.github.com>
2021-10-07 11:31:33 -0300

    Create value_abs.py

---
+++ Week 4/value_abs.py
@@ -0,0 +1,6 @@
+# Program that prompts the user to enter an integer and displays 
+# the absolute value of that integer on one line and then the original integer on the next line
+
+integer = (input('Enter an integer >'))
+print(abs(int(integer)))
+print(integer)
