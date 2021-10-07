6787a4a62edc6d755cf62f74e42acb424b7610c9
Rebeca Raab Bias Ramos <41303692+rebecabramos@users.noreply.github.com>
2021-10-07 11:09:57 -0300

    Create int_for_binary.py

---
+++ Week 3/int_for_binary.py
@@ -0,0 +1,5 @@
+# Program that prompts the user to enter an integer and displays that integer.
+# The program should then prompt the user to enter the integer again and display its binary representation
+
+print(round(float(input('Enter an integer >'))))
+print(bin(int(input('Enter it again >'))))
